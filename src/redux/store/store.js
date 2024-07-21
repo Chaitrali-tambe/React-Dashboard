9a9e454cd918f6e904baac6258be9400a2d8028c
Chaitrali <chaitralitambe30@gmail.com>
2024-07-21 13:35:59 +0530

    api integration in Users page

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import DataReducer from '../reducer/DataReducer'
 
 const store = configureStore({
-    
-})
+    reducer:{
+        data: DataReducer,
+    }
+});
+ 
+export default store;
